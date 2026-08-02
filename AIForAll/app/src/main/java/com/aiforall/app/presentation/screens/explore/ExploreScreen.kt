0164c0fceaf70075bcdfb7ba38b33eaec7af41b5
Abhi^_^ <busaniabhi@gmail.com>
2026-08-02 10:30:15 +0530

    Add ExperimentalMaterial3Api import to ExploreScreen

--- AIForAll/app/src/main/java/com/aiforall/app/presentation/screens/explore/ExploreScreen.kt
+++ AIForAll/app/src/main/java/com/aiforall/app/presentation/screens/explore/ExploreScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
