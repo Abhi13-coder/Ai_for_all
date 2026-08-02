b9e9f32e4570be49c91766320af384cb0fd2aac3
Abhi^_^ <busaniabhi@gmail.com>
2026-08-02 10:25:42 +0530

    Change import from ui.background to foundation.background

--- AIForAll/app/src/main/java/com/aiforall/app/presentation/screens/profile/ProfileScreen.kt
+++ AIForAll/app/src/main/java/com/aiforall/app/presentation/screens/profile/ProfileScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.background
+import androidx.compose.foundation.background
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.aiforall.app.presentation.components.GlassCard
